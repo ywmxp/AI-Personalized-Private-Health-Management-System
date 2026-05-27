ae3b01b545bc1579d517f4a555092eaec6445053
余昌达 <3426877607@qq.com>
2026-05-27 15:08:56 +0800

    修正路由组件导入路径

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home/Home.vue'
-import Login from '../views/Login/Login.vue'
-import Register from '../views/Register/Register.vue'
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
 
 const routes = [
   {
